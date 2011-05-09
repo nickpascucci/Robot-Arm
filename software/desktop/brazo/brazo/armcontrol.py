e85c0d16f395de24c9a210874aa043cbe8927836
Nick Pascucci <npascut1@comcast.net>
2011-05-08 20:14:15 -0600

    Added serial command support to replace various helper functions.

--- software/desktop/brazo/brazo/armcontrol.py
+++ software/desktop/brazo/brazo/armcontrol.py
@@ -5,6 +5,7 @@
 """
 
 import serial
+import time
 
 class ArmControl:
     """
@@ -14,77 +15,100 @@ class ArmControl:
     protocol. Generally, this will be talking to a robot arm over a USB-TTY/UART
     connection.
     """
-	
+    # Listing of commands recognized by the firmware.
+    commands = {"kCOMM_ERROR": "0",
+        "kACK": "1",
+        "kARDUINO_READY": "2",
+        "kERR": "3",
+        "kPOSITION": "4",
+        "kSPEED": "5",
+        "kGRIP": "6",
+        "kCOMPLETE": "7",
+        "kHOLDER2": "8",
+        "kHOLDER3": "9",
+        "kHOLDER4": "10",
+        "kHOLDER5": "11",
+        "kHOLDER6": "12",
+        "kHOLDER7": "13",
+        "kHOLDER8": "14",
+        "kHOLDER9": "15",
+        "kHOLDER10": "16",
+        "set-position": "17", 
+        "set-speed": "18", 
+        "set-grip": "19", 
+        "toggle-led": "20"}
+
     def __init__(self, ser_port):
         """
         Initialize the connection using the given port.
 
         May raise an exception.
         """
+
         self.__serial = serial.Serial(port=ser_port) # Defaults are OK. 8N1 9600b
         try:
             self.__serial.open()
         except serial.SerialException as e:
             raise Exception("Could not open serial port.")
-		
+
     def move_base(self, degrees):
-		"""Rotate the base of the arm by the specified amount."""
-		
-		
+        """Rotate the base of the arm by the specified amount."""
+        
+        
     def move_shoulder(self, degrees):
-		"""Rotate the shoulder of the arm by the specified amount."""
-		
-		
+        """Rotate the shoulder of the arm by the specified amount."""
+        
+        
     def move_elbow(self, degrees):
-		"""Rotate the elbow of the arm by the specified amount."""
-		
-		
+        """Rotate the elbow of the arm by the specified amount."""
+        
+        
     def rotate_wrist(self, degrees):
-		"""Rotate the wrist of the arm by the specified amount."""
-		
-	
+        """Rotate the wrist of the arm by the specified amount."""
+        
+    
     def flex_wrist(self, degrees):
-		"""Flex the wrist of the arm by the specified amount."""
-		
-		
+        """Flex the wrist of the arm by the specified amount."""
+        
+        
     def rotate_gripper(self, degrees):
-		"""Rotate the gripper of the arm by the specified amount."""
-		
-		
+        """Rotate the gripper of the arm by the specified amount."""
+        
+        
     def open_grip(self):
-		"""Open the arm's gripper."""
-		
-		
+        """Open the arm's gripper."""
+        
+        
     def close_grip(self):
-		"""Close the arm's gripper."""
-		
-	
+        """Close the arm's gripper."""
+        
+    
     def read_base_angle(self):
-		"""Read the current base position and return it."""
-		
-		
+        """Read the current base position and return it."""
+        
+        
     def read_shoulder_angle(self):
-		"""Read the current base position and return it."""
-		
-		
+        """Read the current base position and return it."""
+        
+        
     def read_elbow_angle(self):
-		"""Read the current base position and return it."""
-		
-		
+        """Read the current base position and return it."""
+        
+        
     def read_wrist_rotation_angle(self):
-		"""Read the current base position and return it."""
-		
-		
+        """Read the current base position and return it."""
+        
+        
     def read_wrist_flexion_angle(self):
-		"""Read the current base position and return it."""
-		
-		
+        """Read the current base position and return it."""
+        
+        
     def read_gripper_angle(self):
-		"""Read the current base position and return it."""
-		
-		
+        """Read the current base position and return it."""
+        
+        
     def read_gripper_closed(self):
-		"""Read the state of the gripper and return True if closed."""
+        """Read the state of the gripper and return True if closed."""
 
     def get_current_pose(self):
         base = self.read_base_angle()
@@ -96,23 +120,42 @@ class ArmControl:
         grip = self.read_gripper_closed()
         pose = Pose(base, shoulder, elbow, wrist_rot, wrist_flex, grip_rot, grip)
         return pose
-		
+
     def move_to_pose(self, pose):
-		"""Moves the robot into the given pose."""
-		self.move_base(pose.base)
-		self.move_shoulder(pose.shoulder)
-		self.move_elbow(pose.elbow)
-		self.rotate_wrist(pose.wristRot)
-		self.flex_wrist(pose.wristFlex)
-		self.rotate_grip(pose.gripRot)
-		if pose.grip == 0:
-			self.close_grip()
-		else:
-			self.open_grip()
-			
+        """Moves the robot into the given pose."""
+        self.move_base(pose.base)
+        self.move_shoulder(pose.shoulder)
+        self.move_elbow(pose.elbow)
+        self.rotate_wrist(pose.wristRot)
+        self.flex_wrist(pose.wristFlex)
+        self.rotate_grip(pose.gripRot)
+        if pose.grip == 0:
+            self.close_grip()
+        else:
+            self.open_grip()
+            
+    # Writes the command to the serial line in a manner understood by the firmware.
+    def send_command(self, command, *args):
+        cmd_str = ArmControl.commands[command]
+        args_str = ",".join(args)
+        if len(args_str) > 0:
+            args_str = "," + args_str
+        print args_str
+        msg = cmd_str + args_str + ";"
+        print msg
+        bytes = self.__serial.write(msg)
+        print bytes, "written"
+    
+    def read_available(self):
+        num_avail = self.__serial.inWaiting()
+        return self.__serial.read(num_avail)
+
+    def read(self, num):
+        return self.__serial.read(num)
+            
     def close_connection(self):
-		"""Close the serial connection."""
-		self.__serial.close()
+        """Close the serial connection."""
+        self.__serial.close()
 
 class Pose:
     """Stores a pose of the arm."""
@@ -126,11 +169,11 @@ class Pose:
         self.wristFlex = wristFlex
         self.gripRot = gripRot
         self.gripClose = gripClose
-	
+    
 #	def __repr__(self):
-		#"""Print the representation of this object as a string."""
-		
-		
+        #"""Print the representation of this object as a string."""
+        
+        
     def __str__(self):
         """Print the informal representation of this object as a string."""
         str = "<Base: {0}, Shoulder: {1}, Elbow: {2}, Wrist Rotation: {3}, Wrist Flexion: {4}, Grip Rotation: {5}, Grip Closed: {6}>".format(self.base,
@@ -141,14 +184,14 @@ class Pose:
         self.gripRot,
         self.gripClose)
         return str
-		
+        
     def _short_string(self):
         """Return a short string representation of this object."""
         
         if self.gripClose == True:
-        	grip = "Closed"
-       	else:
-       		grip = "Open"
+            grip = "Closed"
+        else:
+            grip = "Open"
         str = "<{0}, {1}, {2}, {3}, {4}, {5}, {6}>".format(self.base,
         self.shoulder,
         self.elbow,
@@ -159,14 +202,31 @@ class Pose:
         return str
         
 def get_available_ports():
-	"""Enumerate available serial ports and return a list of names."""
-	available = []
-	for i in range(256):
-		try:
-		    s = serial.Serial(i)
-		    available.append(s.portstr)
-		    s.close()
-		except serial.SerialException:
-		    pass
-	return available
+    """Enumerate available serial ports and return a list of names."""
+    available = []
+    for i in range(256):
+        try:
+            s = serial.Serial(i)
+            available.append(s.portstr)
+            s.close()
+        except serial.SerialException:
+            pass
+    return available
+    
+if __name__ == "__main__":
+    # Test module
+    ac = ArmControl("/dev/ttyUSB0")
+    print "Checking on Arduino..."
+    ac.send_command("kARDUINO_READY")
+    time.sleep(1)
+    response = ac.read(1)
+    if str(response) is ArmControl.commands["kACK"]:
+        print "Arduino is ready!"
+    else:
+        print "Arduino responded with", response, "which is not 'Arduino ready'."
+    time.sleep(1)
+    print "Toggling led..."
+    ac.send_command("toggle-led", "1", "2")
+    time.sleep(1)
+    ac.close_connection()
 
