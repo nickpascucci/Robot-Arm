64b46730746e6f17c9609dac7b8cd15b5ff9d8c2
Nick Pascucci <npascut1@gmail.com>
2014-06-23 22:42:10 -0400

    Update README and add experiments folder.

---
+++ software/firmware/experiments/Multiplexer/Multiplexer.h
@@ -0,0 +1,22 @@
+/**
+    Multiplexer.h - A library for controlling the 74HC4052 serial multiplexer.
+    Created by Nicholas Pascucci, May 2011
+*/
+#ifndef Multiplexer_h
+#define Multiplexer_h
+
+#include "WProgram.h"
+
+class Multiplexer
+{
+    public:
+        Multiplexer(int a, int b);
+        int select(int channel);
+        
+    private:
+        int _a;
+        int _b;
+        int _chan;
+};
+
+#endif
